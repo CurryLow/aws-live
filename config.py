39c66d8310886c518728759a49434503724d16ba
CurryLow <lowjl-pm24@student.tarc.edu.my>
2025-12-06 12:39:47 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cksc9ewmokwx.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee_db"
+custombucket = "lowjiali-bucket"
+customregion = "us-east-1"
+
